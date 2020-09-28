636acc8267b5c5a1a9b3ea554bc624c45ee895a5
duyanh-y4n <duyanh.y4n.pham@gmail.com>
2020-09-28 15:39:47 +0200

    refactor

--- install_cmakelist.sh
+++ install_cmakelist.sh
@@ -1,3 +1,6 @@
 cd project_template_CMakelists
 find . -path ./cmake -prune -false -o -type f -name 'CMakeLists.txt' -exec cp --parents --verbose {} ../.. \;
 cp -r build ../../
+cp Middlewares/Middlewares.c ../../Middlewares
+cp Drivers/Drivers.c ../../Drivers
+cp build/build.sh ../build
