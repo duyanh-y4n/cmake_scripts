47b20c6ce2f835a78c64d7c0090f079024323475
duyanh-y4n <duyanh.y4n.pham@gmail.com>
2020-09-28 17:22:27 +0200

    add FatFS

--- project_template_CMakelists/build/build.sh
+++ project_template_CMakelists/build/build.sh
@@ -2,6 +2,7 @@ cmake -DMCU=STM32F769xx \
     -DDEBUG_OUTPUT_EN=ON \
     -DSEGGER_SYSVIEW=OFF \
     -DFREERTOS=ON \
+    -DFATFS=ON \
     --graphviz=dependency.dot .. 
 dot -Tpng -o ../docs/dependency.png dependency.dot 
 make -j4 
